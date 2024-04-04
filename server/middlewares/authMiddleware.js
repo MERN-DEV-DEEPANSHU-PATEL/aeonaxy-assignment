d557f946a276900a185bf860dd5e8be9fa9c7126
unknown <pateldeepanshu196@gmail.com>
2024-04-04 19:25:06 +0530

    token error

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,6 +3,10 @@ import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token);
+  console.log(req.cookies);
+  console.log(req.cookie);
+  console.log(req);
   if (!token)
     throw new UnauthenticatedError("authentication invalid Token not Found");
 
